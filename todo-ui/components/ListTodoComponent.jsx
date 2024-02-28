c9604d0858369184074bd960a92b152b94afb630
Onur Haktan <onurhaktan243@gmail.com>
2024-02-28 21:36:39 +0300

    implement deleteTodo endpoints in todo-ui

--- todo-ui/components/ListTodoComponent.jsx
+++ todo-ui/components/ListTodoComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { getAllTodos } from '../src/services/TodoService';
+import { deleteTodo, getAllTodos } from '../src/services/TodoService';
 import { useNavigate } from 'react-router-dom';
 
 const ListTodoComponent = () => {
@@ -30,6 +30,14 @@ const ListTodoComponent = () => {
         navigate(`/update-todo/${id}`)
     }
 
+    function removeTodo(id){
+        deleteTodo(id).then((response) => {
+            listTodos();
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+
     
   return (
     <div className='container'>
@@ -54,6 +62,7 @@ const ListTodoComponent = () => {
                             <td>{todo.completed ? 'Yes' : 'No'}</td>
                             <td>
                                 <button className='btn btn-primary' onClick={() => updateTodo(todo.id)}>Update</button>
+                                <button className='btn btn-danger' onClick={() => removeTodo(todo.id)} style={ {marginLeft: "10px"} }>Delete</button>
                             </td>
                         </tr>
                     )
--- todo-ui/src/services/TodoService.js
+++ todo-ui/src/services/TodoService.js
@@ -9,3 +9,5 @@ export const addTodo = (todo) => axios.post(BASE_REST_API_URL, todo);
 export const getTodoById = (id) => axios.get(BASE_REST_API_URL + '/' + id);
 
 export const updateTodo = (id, todo) => axios.put(BASE_REST_API_URL + '/' + id, todo);
+
+export const deleteTodo = (id) => axios.delete(BASE_REST_API_URL + '/' + id);
